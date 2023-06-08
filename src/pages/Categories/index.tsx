f750494a980a6e92529a395dbec58bf0a9d8152d
Yago Frankley <frankleyyago@outlook.com>
2023-06-08 15:06:59 -0300

    feat: adding hashlink to sections

--- src/pages/Categories/index.tsx
+++ src/pages/Categories/index.tsx
@@ -22,14 +22,30 @@ const Categories = () => {
   if (actionGames && fightGames && rpgGames && simulationGames && sportGames) {
     return (
       <>
-        <ProductList games={actionGames} title="Acao" background="black" />
-        <ProductList games={sportGames} title="Esportes" background="gray" />
-        <ProductList games={fightGames} title="Luta" background="black" />
-        <ProductList games={rpgGames} title="RPG" background="gray" />
+        <ProductList
+          games={actionGames}
+          title="Acao"
+          background="black"
+          id="action"
+        />
+        <ProductList
+          games={sportGames}
+          title="Esportes"
+          background="gray"
+          id="sports"
+        />
+        <ProductList
+          games={fightGames}
+          title="Luta"
+          background="black"
+          id="fight"
+        />
+        <ProductList games={rpgGames} title="RPG" background="gray" id="rpg" />
         <ProductList
           games={simulationGames}
           title="Simulacao"
           background="black"
+          id="simulation"
         />
       </>
     )
